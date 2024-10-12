3734bb23d875d7e72db0e69f86e3893a065b8af5
nabeelmohamed <mohamednabeel321@gmail.com>
2024-10-12 06:18:20 +0000

    added index.php

---
+++ mysite/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Project</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>Welcome to my PHP project deployed with Jenkins CI/CD pipeline.</p>
+</body>
+</html>
+
